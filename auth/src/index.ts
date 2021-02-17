723860b72340bd612fdcfc37e8a33d19d8801a6f
enguemeta <emmanuel.nguemeta-fomekong.1@ulaval.ca>
2021-02-16 20:33:18 -0500

    update index.ts on auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import { app } from "./app";
 
 
 const start: any = async () => {
-
+   console.log("Starting up...");
   if(!process.env.JWT_KEY) {
      throw new Error("JWT_KEY must be defined");
   }
